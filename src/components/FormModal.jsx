6921cb4b4578d46971eeaaabd3c179147cc79770
DivyanshiTout <divyanshi.tout@gmail.com>
2025-04-09 18:43:30 +0530

    update the code

--- src/components/FormModal.jsx
+++ src/components/FormModal.jsx
@@ -1,21 +1,10 @@
 "use client"
-import { useState } from "react";
 
 const FormModal = ({show,closeModal}) => {
-    // const [show, setShow] = useState(false);
     if (!show) return null;
 
     return (
         <>
-            {/* Modal toggle button */}
-            {/* <button
-                onClick={() => setShow(true)}
-                className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-                type="button"
-            >
-                Toggle modal
-            </button> */}
-
             {/* Modal */}
             {show && (
                 <div
@@ -134,7 +123,7 @@ const FormModal = ({show,closeModal}) => {
                         </div>
                     </div>
                 </div>
-            )} 
+                )} 
         </>
     );
 };
